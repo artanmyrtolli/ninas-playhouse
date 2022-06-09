8ea9ba06c7edf64649bcae8d87772ca6a888bfec
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-06-09 05:14:10 -0400

    Add functionality to browse page, pulling toys from local API

--- src/Components/App.js
+++ src/Components/App.js
@@ -21,7 +21,6 @@ function App() {
       </Route>
       <Route path="/browse">
         <ToyContainer />
-        <h1>browse</h1>
       </Route>
       <Route path="/login">
         <Login />
--- src/Components/ToyContainer.js
+++ src/Components/ToyContainer.js
@@ -8,13 +8,11 @@ const ToyContainer = () => {
     const fetchToys = async () => {
         let response = await fetch('http://localhost:3001/toys')
         response = await response.json()
-        console.log(response)
         setToys(response.map(toy => {
             return (
                 <Toy 
                 key={toy.id}
                 name={toy.name}
-                type={toy.type}
                 description={toy.description}
                 ages={toy.ages}
                 img={toy.img}/>
